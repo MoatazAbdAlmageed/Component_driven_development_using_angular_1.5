50f9da181991020366837366e1b63f9cdcb37aa2
Moataz <m.mohammedy@tie.com.eg>
2016-12-26 07:33:51 +0200

    ready for interview

--- app/components/employeesComponent.js
+++ app/components/employeesComponent.js
@@ -2,9 +2,9 @@
  * Created by Moataz on 12/25/16.
  */
 
-appModule.controller('employeesCTRL', function ($http,$scope) {
+appModule.controller('employeesCTRL', function ($http, $scope) {
     $http.get('data/employee.json').then(function (response) {
-       $scope.employees = response.data;
+        $scope.employees = response.data;
     })
 })
 appModule.component('employeesComponent', {
